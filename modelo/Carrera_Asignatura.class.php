ec1c2034df809ee6e2bcc6b2e7f07597f5d5acef
Ramiro2709 <vargasramiro99@gmail.com>
2019-11-07 00:07:21 -0300

    comentario

--- modelo/Carrera_Asignatura.class.php
+++ modelo/Carrera_Asignatura.class.php
@@ -3,8 +3,11 @@ include_once 'ModeloDatosGenerico.php';
 
 class Carrera_Asignatura extends ModeloDatosGenerico{
     protected $id;
+    protected $id_asignatura;
+    protected $id_carrera;
     protected $asignatura;
     protected $carrera;
+    
     function __construct($array) {
         parent::mapeoArrayAtributos($array);
     }
@@ -13,8 +16,24 @@ class Carrera_Asignatura extends ModeloDatosGenerico{
         return $this->id;
     }
 
+    function getId_asignatura() {
+        return $this->id_asignatura;
+    }
+
+    function getId_carrera() {
+        return $this->id_carrera;
+    }
+
     function setId($id) {
         $this->id = $id;
+    }
+
+    function setId_asignatura($id_asignatura) {
+        $this->id_asignatura = $id_asignatura;
+    }
+
+    function setId_carrera($id_carrera) {
+        $this->id_carrera = $id_carrera;
     }
 
     function getAsignatura() {
