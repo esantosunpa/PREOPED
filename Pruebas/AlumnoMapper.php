82d72ded367b5bef45da418bbb63bde3bccb3edf
Ramiro2709 <vargasramiro99@gmail.com>
2019-11-24 17:40:49 -0300

    Update AlumnoMapper.php

--- Pruebas/AlumnoMapper.php
+++ Pruebas/AlumnoMapper.php
@@ -6,16 +6,17 @@ error_reporting(E_ALL);
 
 // Prueba
 include_once '../modelo/AlumnoMapper.php';
+include_once '../modelo/Alumno.class.php';
 
 $AMaper = new AlumnoMapper();
 
 //Prueba con resultado esperado = DATOS
-$AlumnoValido = $AMaper->findById(1);
+$AlumnoValido = new Alumno($AMaper->findById(1));
 
 //Prueba con resultado esperado = NULL
 $resultadoAlumnoNoExistente = $AMaper->findById(-1);
 
 
-$familiares = $AMaper->findFamiliares(1);
+$familiares = $AMaper->findFamiliares($AlumnoValido->getId());
 var_dump($familiares);
 
